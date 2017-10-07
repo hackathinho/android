279bf0e7991f59007789cac0484dbe49f86ca2d4
newhouse <nhitbh@gmail.com>
2017-10-07 15:27:19 +0200

    Implemented method to get the user nearest locations

---
+++ domain/src/main/java/com/smartjump/domain/interactor/BaseInteractor.java
@@ -0,0 +1,39 @@
+package com.smartjump.domain.interactor;
+
+import com.smartjump.domain.MainThread;
+import com.smartjump.domain.ThreadExecutor;
+
+import javax.inject.Inject;
+
+import io.reactivex.Observable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ *
+ */
+abstract class BaseInteractor<T, P> {
+
+    private final MainThread mainThread;
+    private final ThreadExecutor threadExecutor;
+    private final CompositeDisposable compositeDisposable;
+
+    @Inject
+    BaseInteractor(MainThread mainThread, ThreadExecutor threadExecutor) {
+        this.mainThread = mainThread;
+        this.threadExecutor = threadExecutor;
+        this.compositeDisposable = new CompositeDisposable();
+    }
+
+    abstract Observable<T> result(P params);
+
+    public void execute(DisposableObserver<T> subscriber, P params) {
+        Observable<T> observable = result(params).subscribeOn(mainThread.scheduler()).observeOn(Schedulers.from(threadExecutor));
+        compositeDisposable.add(observable.subscribeWith(subscriber));
+    }
+
+    public void close() {
+        if (!compositeDisposable.isDisposed()) compositeDisposable.dispose();
+    }
+}
---
+++ domain/src/main/java/com/smartjump/domain/interactor/GetNearStations.java
@@ -0,0 +1,29 @@
+package com.smartjump.domain.interactor;
+
+import com.smartjump.domain.DataRepository;
+import com.smartjump.domain.MainThread;
+import com.smartjump.domain.ThreadExecutor;
+import com.smartjump.domain.model.Jump;
+import com.smartjump.domain.model.UserLocation;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+
+/**
+ *
+ */
+public class GetNearStations extends BaseInteractor<List<Jump>, UserLocation> {
+
+    private final DataRepository dataRepository;
+
+    public GetNearStations(MainThread mainThread, ThreadExecutor threadExecutor, DataRepository dataRepository) {
+        super(mainThread, threadExecutor);
+        this.dataRepository = dataRepository;
+    }
+
+    @Override
+    Observable<List<Jump>> result(UserLocation params) {
+        return dataRepository.userLocation(params);
+    }
+}
